f7c54ecad2f721225c0b65406326bce23cace086
AgneIlg <agneilginyte@yahoo.com>
2024-02-26 17:15:45 +0200

    Initial data

--- js/data/data.js
+++ js/data/data.js
@@ -1,5 +1,46 @@
 const data = {
-    
+    tasks: [
+        {
+            title: 'Reikia pasidaryti arbatos',
+            description: 'Reikia jog butu slapia ir silta',
+            status: 'done',
+        },
+        {
+            title: 'Reikia pasidaryti arbatos',
+            description: 'Reikia jog butu slapia ir silta',
+            status: 'in-progress',
+        },
+
+        {
+            title: 'Reikia pasidaryti arbatos',
+            description: 'Reikia jog butu slapia ir silta',
+            status: 'review',
+        },
+        {
+            title: 'Reikia pasidaryti arbatos',
+            description: 'Reikia jog butu slapia ir silta',
+            status: 'todo',
+        }
+        
+    ],
+    columns: [
+        {
+            title: 'Task ready',
+            status: 'todo',
+        },
+        {
+            title: 'In progress',
+            status: 'in-progress',
+        },
+        {
+            title: 'Needs review',
+            status: 'review',
+        },
+        {
+            title: 'Done',
+            status: 'done',
+        },
+    ]
 };
 
 export { data };
